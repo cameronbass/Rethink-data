cf98f6f1c30ba1cb3da3c4c5c82e9934f9a80345
Cameron Bass <cam@collectiveidea.com>
2017-03-23 20:14:23 -0400

    add pry to the project

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,21 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    binding.pry
+    @article = Article.create(article_params)
 
+    if @article.valid?
+      redirect_to new_article_path
+      flash[:notice] = "You've successfully create the article"
+    else
+      render :new
+      flash[:error] = "Something went wrong"
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :source, :abstract)
   end
 end
